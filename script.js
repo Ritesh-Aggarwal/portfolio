fe8d9146824fe7f9608bc78368410c1bdd1d64d3
Ritesh-Aggarwal <57039447+Ritesh-Aggarwal@users.noreply.github.com>
2021-06-02 17:46:24 +0530

    typewriter animation

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // onload animation
+setTimeout(function(){
+	var app = document.getElementById('app');
 
-$("#hi").hide().fadeIn(1000);
-$("#name").hide().fadeIn(1500);
+var typewriter = new Typewriter(app,{delay:90,cursor:'!'});
+
+typewriter
+.typeString("Hi")
+.pauseFor(1000)
+.deleteAll()
+.typeString("My name is Ritesh Aggarwal")
+.start();
+},100);
 
 // scroll Animation
 
@@ -9,6 +18,7 @@ var slideUp = {
     distance: '100px',
     origin: 'bottom',
     opacity: null,
+	easing : "ease-in"
 };
 ScrollReveal().reveal("#skills",slideUp);
 ScrollReveal().reveal("#projects",slideUp);
