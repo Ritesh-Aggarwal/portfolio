8c7e78056efe168e7bdd3ecee35d069e41911546
Ritesh-Aggarwal <riteshaggarwal2001@gmail.com>
2022-02-24 21:20:00 +0530

    revert to initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,8 @@
 import '../styles/globals.css'
-import '../styles/index.scss'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import Navbar from '../components/Navbar/Navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <main className="app">
-      <Head>
-        <title>Ritesh Aggarwal</title>
-      </Head>
-      <Navbar active={pageProps?pageProps.active:'INTRO'}/>
-      <Component {...pageProps} />
-    </main>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
